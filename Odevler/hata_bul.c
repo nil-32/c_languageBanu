b520343902b876ab38b2b90f5974639364278c39
nil <113453210+nil-32@users.noreply.github.com>
2022-12-07 08:29:40 -0800

    Update hata_bul.c

--- Odevler/hata_bul.c
+++ Odevler/hata_bul.c
@@ -1,16 +1,17 @@
-#include <stdion.h>
+#include <stdio.h>
 int main()
 {
-    char name[0][10], i
-    printf("Enter the 3 name :\n");
+    char name[3][10];
+    int i;
+    printf("Enter the 3 name \n");
     for (i = 0; i < 3; i++)
     {
-        printf("Enter the sring :");
+        printf("Enter the string :");
         scanf("%s", &name[i]);
     }
     for (i = 0; i < 3; i++)
     {
         printf("%s\n", &name[i]);
     }
-    return 10;
+    return 0;
 }
